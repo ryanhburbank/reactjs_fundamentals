5d66c75440fe7682a773b22778c577d9d53fe2c4
Ryan Burbank <rburbank@streamsend.com>
2016-06-05 11:25:22 -0700

    Add Results route and Results and ResultsContainer components, update githubHelpers to calculateScores for each player

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,7 +8,36 @@ var axios = require('axios');
 function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username);
   // return axios.get('https://api.github.com/users/' + username + param);
-}
+};
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + '?per_page=100')
+  // return axios.get('https://api.github.com/users/' + username + '/repos' + param + 'per_page=100')
+};
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count;
+  }, 0);
+};
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+  .then(getTotalStars)
+  .then(function (totalStars) {
+    return {
+      followers: player.followers,
+      totalStars: totalStars
+    };
+  })
+};
+
+function calculateScores (players) {
+  return [
+    (players[0].followers * 3) + players[0].totalStars,
+    (players[1].followers * 3) + players[1].totalStars
+  ];
+};
 
 var helpers = {
   getPlayersInfo: function(players) {
@@ -19,9 +48,18 @@ var helpers = {
       return info.map(function(user) {
         return user.data;
       });
-    }).catch(function (err){
+     })
+    .catch(function (err){
       console.warn('Error in getPlayersInfo: ', err);
     });
+  },
+  battle: function(playersArray) {
+    var playerOneData = getPlayersData(playersArray[0]);
+    var playerTwoData = getPlayersData(playersArray[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+    .then(calculateScores)
+    .catch(function (err) { console.warn('Error in getPlayersInfo: ', err); })
   }
 };
 
