6cc7e3ca9746eac5fa17d9aaa43fb45719c876a2
Ryan Burbank <rburbank@streamsend.com>
2016-06-05 13:52:25 -0700

    Add css loaders to webpack, add transition animations

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,18 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 function Main (props) {
+  //keys are given to each item in a list, which is then used by React to keep track of their order
+  //cloneElement allows you to take a ReactComponent and give it new props
   return (
     <div className='main-container'>
-      {props.children}
+      <ReactCSSTransitionGroup
+        transitionName="appear"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
+          {React.cloneElement(props.children, {key: props.location.pathname})}
+      </ReactCSSTransitionGroup>
     </div>
   )
 };
