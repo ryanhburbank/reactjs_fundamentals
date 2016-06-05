50f57890ba8fcb1c0694f8177c808e91b7302a95
Ryan Burbank <rburbank@streamsend.com>
2016-06-04 17:14:32 -0700

    Remove Avatar and all sub-components, add react-router, add Home, Main, Prompt & PromptContainer components

--- app/index.js
+++ app/index.js
@@ -1,11 +1,6 @@
-var USER_DATA = {
-  name: 'Ryan Burbank',
-  username: 'ryanhburbank',
-  image: 'https://avatars2.githubusercontent.com/u/3589036?v=3&s=140'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
 /* (ensure that every component you build meets the following criteria)
   Focused
@@ -15,48 +10,7 @@ var ReactDOM = require('react-dom');
   Testable
 */
 
-var ProfilePic = React.createClass({
-  render: function() {
-    return (
-      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-    )
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
-
-
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
