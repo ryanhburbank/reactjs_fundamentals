5c898080afdc6d79eee80b40e11d8a7bd31f97e8
Ryan Burbank <rburbank@streamsend.com>
2016-06-04 19:46:56 -0700

    Add UserDetails and UserDetailsWrapper components, finish ui for ConfirmBattle component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,5 +1,9 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('../components/UserDetails');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
 
 // good way to check props
 // function puke (object) {
@@ -10,12 +14,38 @@ function ConfirmBattle (props) {
     return (
       props.isLoading === true
       ? <p> LOADING... </p>
-      : <p>Confirm Battle </p>
-      // : <p>Confirm Battle: {puke(props)} </p>
+      : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+          <h1>Confirm Players</h1>
+          <div className="col-sm-8 col-sm-offset-2">
+            <UserDetailsWrapper header='Player 1'>
+              <UserDetails info={props.playersInfo[0]} />
+            </UserDetailsWrapper>
+            <UserDetailsWrapper header='Player 2'>
+              <UserDetails info={props.playersInfo[1]} />
+            </UserDetailsWrapper>
+          </div>
+          <div className='col-sm-8 col-sm-offset-2'>
+            <div className='col-sm-12' style={styles.space}>
+              <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+                Initiate Battle
+              </button>
+            </div>
+            <div className='col-sm-12' style={styles.space}>
+              <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>
+                Reselect Players
+              </button>
+              </Link>
+            </div>
+          </div>
+        </div>
     )
 };
 
 ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 };
 
 module.exports = ConfirmBattle;
